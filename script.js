b5c1eca36a6511d5191a5a49bc1bac9a2432f149
Kenneth <kenneth.kristopher27@gmail.com>
2022-04-17 16:12:00 +0700

    add brands

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav__links');
+const navLinks = document.querySelector('.nav__li');
+const brands = document.querySelector('.nav--brand');
 
 hamburger.addEventListener('click', ()=>{
     nav.classList.toggle('active');
+})
+
+navLinks.addEventListener('click', ()=>{
+    brands.classList.toggle('active');
 })
