e77669a770d6e2ec31360721223d1b636c810387
Kenneth <kenneth.kristopher27@gmail.com>
2022-04-17 15:56:17 +0700

    added nav animation

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav__links');
 
 hamburger.addEventListener('click', ()=>{
-    nav.classList.toggle('hidden');
+    nav.classList.toggle('active');
 })
