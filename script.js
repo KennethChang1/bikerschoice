aae259164ea5a5288cc61f234154e094d15981a7
Kenneth <kenneth.kristopher27@gmail.com>
2022-04-19 14:12:24 +0700

    added animation

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav__links');
 const navLinks = document.querySelectorAll('.nav__li');
 const brands = document.querySelectorAll('.nav--brand');
+const links = document.querySelectorAll('.nav__sub');
 
 hamburger.addEventListener('click', () => {
     nav.classList.toggle('active');
     close();
+    displayLink();
 })
 
 navLinks.forEach((element, index) => {
@@ -18,4 +20,22 @@ function close(){
     brands.forEach((element, index) => {
         brands[index].classList.remove('active');
     });
+}
+
+function displayLink(){
+    navLinks.forEach((element)=>{
+        if(element.style.animation){
+            element.style.animation = "";
+        } else{
+            element.style.cssText = 'animation: link 0s forwards; animation-delay: 0.2s;';
+        }
+    });
+
+    // links.forEach((element)=>{
+    //     if(element.style.animation){
+    //         element.style.animation = "";
+    //     } else{
+    //         element.style.cssText = 'animation: link 1s forwards; animation-delay: 0.2s;';
+    //     }
+    // });
 }
