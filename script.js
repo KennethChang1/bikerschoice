4fd9d9ce97ba2d7947a6f10dbc6589503997083f
Kenneth <kenneth.kristopher27@gmail.com>
2022-05-21 21:57:35 +0700

    implement no scroll when nav bar is open

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav-links');
 const close = document.querySelector('.close');
+const body = document.querySelector('body');
 
 hamburger.addEventListener('click', ()=>{
     nav.classList.add('nav-links-active');
+    body.classList.add('no-scroll');
 })
 
 close.addEventListener('click', ()=>{
     nav.classList.remove('nav-links-active');
+    body.classList.remove('no-scroll');
 })
