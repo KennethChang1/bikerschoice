9a938805c4c9d19aab8b4815cbc59f82a79f758c
Kenneth <kenneth.kristopher27@gmail.com>
2022-05-21 21:24:57 +0700

    added navbar

--- script.js
+++ script.js
@@ -1,41 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav__links');
-const navLinks = document.querySelectorAll('.nav__li');
-const brands = document.querySelectorAll('.nav--brand');
-const links = document.querySelectorAll('.nav__sub');
+const nav = document.querySelector('.nav-links');
+const close = document.querySelector('.close');
 
-hamburger.addEventListener('click', () => {
-    nav.classList.toggle('active');
-    close();
-    displayLink();
+hamburger.addEventListener('click', ()=>{
+    nav.classList.add('nav-links-active');
 })
 
-navLinks.forEach((element, index) => {
-    navLinks[index].addEventListener('click', () => {
-        brands[index].classList.toggle('active');
-    });
-});
-
-function close(){
-    brands.forEach((element, index) => {
-        brands[index].classList.remove('active');
-    });
-}
-
-function displayLink(){
-    navLinks.forEach((element)=>{
-        if(element.style.animation){
-            element.style.animation = "";
-        } else{
-            element.style.cssText = 'animation: link 0s forwards; animation-delay: 0.2s;';
-        }
-    });
-
-    // links.forEach((element)=>{
-    //     if(element.style.animation){
-    //         element.style.animation = "";
-    //     } else{
-    //         element.style.cssText = 'animation: link 1s forwards; animation-delay: 0.2s;';
-    //     }
-    // });
-}
+close.addEventListener('click', ()=>{
+    nav.classList.remove('nav-links-active');
+})
