0a29d4a31df429b7afa13f14c1f0e111bb43baf3
Kenneth <kenneth.kristopher27@gmail.com>
2022-06-14 13:44:43 +0700

    changed stories

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ const nav = document.querySelector('.nav-links');
 const close = document.querySelector('.close');
 const body = document.querySelector('body');
 
-hamburger.addEventListener('click', ()=>{
+hamburger.addEventListener('click', () => {
     nav.classList.add('nav-links-active');
-    body.classList.add('no-scroll');
+    body.style.position = 'fixed';
+    body.style.width = `100%`;
 })
 
-close.addEventListener('click', ()=>{
+close.addEventListener('click', () => {
     nav.classList.remove('nav-links-active');
     body.classList.remove('no-scroll');
+    body.style.position = '';
+    body.style.width = ``;
 })
